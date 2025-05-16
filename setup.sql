cbfa6471013d9761177cc61a38fb28888e4cfd05
Maurício Sardinha <mauriciogabrielbs@hotmail.com>
2025-05-16 10:32:17 +0100

    Database Progress

--- setup.sql
+++ setup.sql
@@ -7,31 +7,47 @@ BEGIN TRANSACTION;
 PRAGMA foreign_keys = ON;
 
 DROP TABLE IF EXISTS user_registry;
-DROP TABLE IF EXISTS user_info;
 DROP TABLE IF EXISTS user_language;
 DROP TABLE IF EXISTS language;
 DROP TABLE IF EXISTS service;
 DROP TABLE IF EXISTS user_service;
 
-/*joint registry table with info*/
+
 CREATE TABLE user_registry
 (
     user_id INTEGER PRIMARY KEY AUTOINCREMENT,
     password_hash TEXT,
-    access_level TEXT
-);
+    access_level TEXT,
 
-CREATE TABLE user_info
-(
-    user_id INTEGER,
+
+    user_picture BLOB DEFAULT NULL,
+
     name TEXT,
-    email TEXT,
+    email TEXT UNIQUE,
     join_date DATETIME,
-    country TEXT,
-    user_rating REAL
+
+    aboutme TEXT DEFAULT NULL,
+
 
     PRIMARY KEY(user_id),
     FOREIGN KEY(user_id) REFERENCES user_registry(user_id)
+);
+
+CREATE TABLE skill
+(
+    skill_name TEXT, /* python */
+
+    PRIMARY KEY(skill_name)
+);
+
+CREATE TABLE user_skills
+(
+    user_id INTEGER,
+    skill_name TEXT,
+    
+    PRIMARY KEY(user_id, skill_name),
+    FOREIGN KEY(user_id) REFERENCES user_registry(user_id),
+    FOREIGN KEY(skill_name) REFERENCES skill(skill_name)
 );
 
 CREATE TABLE language
@@ -40,7 +56,7 @@ CREATE TABLE language
     lang_name TEXT, /* Portuguese */
 
     PRIMARY KEY(lang_code)
-)
+);
 
 CREATE TABLE user_language
 (
@@ -48,32 +64,51 @@ CREATE TABLE user_language
     lang_code TEXT
 
     PRIMARY KEY(user_id, lang_code),
-    FOREIGN KEY(user_id) REFERENCES user_info(user_id),
+    FOREIGN KEY(user_id) REFERENCES user_registry(user_id),
     FOREIGN KEY(lang_code) REFERENCES language(lang_code)
 );
 
-
 CREATE TABLE service
 (
-    service_id INTEGER,
+    service_id INTEGER PRIMARY KEY AUTOINCREMENT,
     freelancer_id INTEGER,
-    serviced_price REAL,
-    service_info TEXT,
-    etc BLOB,
 
-    PRIMARY KEY(service_id),
+    service_title TEXT,
+    service_price REAL,
+    service_info TEXT,
+
+    service_picture BLOB,
+
     FOREIGN KEY(freelancer_id) REFERENCES user_registry(user_id)
 );
 
-CREATE TABLE user_service
+CREATE TABLE purchase
 (
-    user_id INTEGER,
-    service_id TEXT,
+    purchase_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    client_id INTEGER,
+    service_id INTEGER,
     completed BOOLEAN,
 
-    PRIMARY KEY(user_id, service_id),
-    FOREIGN KEY(user_id) REFERENCES user_info(user_id),
+    review_text TEXT DEFAULT NULL,
+    review_rating INTEGER DEFAULT NULL,
+
+    FOREIGN KEY(client_id) REFERENCES user_registry(user_id),
     FOREIGN KEY(service_id) REFERENCES service(service_id)
 );
+
+CREATE TABLE message
+(
+    client_id INTEGER,
+    service_id INTEGER,
+
+    message_text TEXT,
+    date_time DATETIME,
+    is_reply BOOLEAN,
+
+    PRIMARY KEY(client_id, service_id, message_text),
+    FOREIGN KEY(client_id) REFERENCES purchase(client_id),
+    FOREIGN KEY(service_id) REFERENCES service(service_id)
+); 
+ 
 
 COMMIT;
