2b43e987f40a3ed22ea6139c232353dcfa35e890
Maurício Sardinha <mauriciogabrielbs@hotmail.com>
2025-05-25 17:34:51 -0300

    removed csrf verification from reviews

--- action/submit_review.php
+++ action/submit_review.php
@@ -6,13 +6,7 @@ require_once __DIR__ . '/../database/service_class.php';
 $session = Session::getInstance();
 require_once '../utils/csrf.php';
 $data = json_decode(file_get_contents('php://input'), true);
-$csrf_token = $data['csrf_token'] ?? '';
 
-if (!CSRF::verifyCSRF($csrf_token)) {
-    http_response_code(403);
-    echo json_encode(['success' => false, 'error' => 'Invalid CSRF token']);
-    exit;
-}
 if (!$session->isLoggedIn()) {
     http_response_code(403);
     echo json_encode(['success' => false, 'error' => 'Not logged in']);
