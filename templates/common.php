aef36911a4b8f7ee6a44ee1f0bb2bd069c684b24
Maurício Sardinha <mauriciogabrielbs@hotmail.com>
2025-05-25 13:37:30 -0300

    Solved Uncaught Error: Class CSRF not found

--- templates/common.php
+++ templates/common.php
@@ -1,5 +1,6 @@
 <?php 
 require_once __DIR__ . '/../utils/session.php';
+require_once '../utils/csrf.php';
 function drawHeader() { 
   $session = Session::getInstance();
   $loggedIn = $session->isLoggedIn();
